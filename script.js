e7bab324c17d148b5d74475a3b3dd6a29b8dca1a
P.PAVAN KUMAR <105867367+pavankumar6123@users.noreply.github.com>
2022-08-29 17:15:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let element = document.getElementById('counter');
+let count = 0;
+
+setInterval ( ()=> {
+    if(count<=3484)
+    element.innerText = count;
+    count++;
+},10);
+
+
+
+let element1 = document.getElementById('pavan');
+let count1 = 0;
+
+setInterval ( ()=> {
+    if(count1<=7000)
+    element1.innerText = count1;
+    count1++;
+},100);
+
+
+
+
+let element2 = document.getElementById('kumar');
+let count2 = 0;
+
+setInterval ( ()=> {
+    if(count2<=8000)
+    element2.innerText = count2;
+    count2++;
+},100);
+
+
+
+let element3 = document.getElementById('tiger');
+let count3 = 0;
+
+setInterval ( ()=> {
+    if(count3<=100)
+    element3.innerText = count3;
+    count3++;
+},1000);
+
+
+
+let element4 = document.getElementById('raj');
+let count4 = 0;
+
+setInterval ( ()=> {
+    if(count4<=10000)
+    element4.innerText = count4;
+    count4++;
+},10);
